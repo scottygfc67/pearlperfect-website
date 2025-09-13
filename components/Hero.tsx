117f2209699ab00168f51b77a1845d701e0f85b3
Scott Briggs <briggsscott12@gmail.com>
2025-09-13 13:56:15 +0100

    hero changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
       {/* Background image */}
       <div className="absolute inset-0 -z-10">
         <Image
-          src="/gradhero.jfif"
+          src="/grad.jpeg"
           alt="Smiling person with bright teeth"
           priority
           fill
